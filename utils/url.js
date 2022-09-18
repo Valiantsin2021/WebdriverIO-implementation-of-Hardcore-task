3833ca48f16a55bc3ca35b68921d5312826a0e7a
Valiantsin2021 <Valiantsin2021@users.noreply.github.com>
2022-09-18 07:41:37 +0000

    Automated deployment: Sun Sep 18 07:41:37 UTC 2022 40c83e223a1f27831c01432d6878454b1e2859fe

--- utils/url.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    dev: "http://www.google.com/intl/en/",
-    qa: "http://www.google.com/intl/ru/", 
-    stage: "http://www.google.com/intl/es/",
-    prod: "http://www.google.com/intl/de/",
-}
